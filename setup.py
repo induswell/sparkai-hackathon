e71079a91e9ddd646e7c4c34b0869aa18f401ed2
induswell <induswell@gmail.com>
2020-06-08 12:41:52 -0500

    initial project template commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Spark AI Summit 2020 Hackathon for Social Good',
+    author='Team United',
+    license='MIT',
+)
